9b3530725549ffc392d48b6270fbcd557766da32
artdgn <arthurdgn@gmail.com>
2020-07-15 10:05:47 +1000

    add backend choice and listener on options change

--- extension/js/filter.js
+++ extension/js/filter.js
@@ -28,70 +28,92 @@ function collectTexts(elements) {
 }
 
 
-function markByBackend(elements, styling) {
+function markByBackend(elements) {
+    chrome.storage.sync.get({
+        backend: 'default',
+    }, function(stored) {
+        backend = stored.backend;
+        console.log("stored backend setting: " + backend);
+        if (backend == "random") {
+            randomValuesBackend(elements);
+        } else {
+            pythonSentimentBackend(elements);
+        };
+    });
+}
+
+
+function pythonSentimentBackend(elements) {
     texts = collectTexts(elements);
     $.ajax({
         type: 'post',
         url: 'http://localhost:8000/sentiment/',
         data: JSON.stringify({'texts': texts}),
         success: function(values, status) {
-            markByValues(elements, values, styling)
+            markByValues(elements, values);
         },
         error: function(xhr, status, error) {
-          alert(xhr.responseText);
+            alert(xhr.responseText);
         }
     });
 }
 
 // random marks to simluate a backend without running one
-function markByRandom(elements, styling) {
+function randomValuesBackend(elements) {
     values = [];
     elements.each(function (index) {
         values[index] = Math.random();
     });
-    markByValues(elements, values, styling);
+    markByValues(elements, values);
 }
 
 
-function markByValues(elements, neg_values, styling) {
+function markByValues(elements, neg_values) {
     neg_threshold = 0.85;
     pos_threshold = 0.35;
     min_opacity = 0.4
-    elements.each(function (index) {
-        neg_score = neg_values[index];
 
-        // opacity
-        if (neg_score >= neg_threshold) {
-            norm_score = (neg_score - neg_threshold) / (1 - neg_threshold);
-            opacity = 1 - norm_score * (1 - min_opacity);
-            $(this).css('opacity', opacity);
-        }
+    chrome.storage.sync.get({
+       styling: 'default',
+    }, function(stored) {
+       console.log("stored styling setting: " + stored.styling);
 
-        // color
-        if (styling != "nocolors") {
-            if (neg_score >= neg_threshold) {
-                norm_score = (neg_score - neg_threshold) / (1 - neg_threshold);
-                color_val = Math.round(255 * norm_score);
-                $(this).css('background-color',
-                            `rgba(255, ${255 - color_val}, ${255 - color_val})`);
-            } else if (neg_score < pos_threshold) {
-                norm_score = (pos_threshold - neg_score) / pos_threshold;
-                color_val = Math.round(255 * norm_score);
-                $(this).css('background-color',
-                            `rgb(${255 - color_val}, 255, ${255 - color_val})`);
-            }
-        }
+       styling = stored.styling;
 
+       elements.each(function (index) {
+           neg_score = neg_values[index];
+
+           // opacity
+           if (neg_score >= neg_threshold) {
+               norm_score = (neg_score - neg_threshold) / (1 - neg_threshold);
+               opacity = 1 - norm_score * (1 - min_opacity);
+               $(this).css('opacity', opacity);
+           }
+
+           // color
+           if (styling != "nocolors") {
+               if (neg_score >= neg_threshold) {
+                   norm_score = (neg_score - neg_threshold) / (1 - neg_threshold);
+                   color_val = Math.round(255 * norm_score);
+                   $(this).css('background-color',
+                               `rgba(255, ${255 - color_val}, ${255 - color_val})`);
+               } else if (neg_score < pos_threshold) {
+                   norm_score = (pos_threshold - neg_score) / pos_threshold;
+                   color_val = Math.round(255 * norm_score);
+                   $(this).css('background-color',
+                               `rgb(${255 - color_val}, 255, ${255 - color_val})`);
+               }
+           } else {
+               $(this).css('background-color', 'white');
+           }
+       });
     });
 }
 
 
-chrome.storage.sync.get({
- styling: 'default',
-}, function(stored) {
-   console.log("Styling setting stored is: " + stored.styling);
-   elements = findElements();
-   markByBackend(elements, stored.styling)
-//   markByRandom(elements, stored.styling)
- });
+elements = findElements();
+markByBackend(elements)
+chrome.storage.onChanged.addListener(function(changes) {
+    markByBackend(elements);
+})
 chrome.runtime.sendMessage({}, function(response) {});
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,28 +1,34 @@
 function saveOptions() {
   var selectedStyling = document.getElementById('selectedStyling').value;
+  var selectedBackend = document.getElementById('selectedBackend').value;
 
   chrome.storage.sync.set({
-    styling: selectedStyling
+    styling: selectedStyling,
+    backend: selectedBackend
   }, function() {
-    console.log('Styling selected - ' + selectedStyling)
+    console.log('Styling:' + selectedStyling);
+    console.log('Backend:' + selectedBackend);
   });
 }
 
 function getOptions(callback) {
   chrome.storage.sync.get({
     styling: 'default',
+    backend: 'default'
   }, function(stored) {
     document.getElementById('selectedStyling').value = stored.styling;
-    callback(stored.styling);
-    return stored.styling;
+    document.getElementById('selectedBackend').value = stored.backend;
+    callback(stored);
   });
 }
 
 function restoreOptions() {
-  getOptions(function(styling) {
-    document.getElementById('selectedStyling').value = styling;
+  getOptions(function(stored) {
+    document.getElementById('selectedStyling').value = stored.styling;
+    document.getElementById('selectedBackend').value = stored.backend;
   });
   document.getElementById('selectedStyling').addEventListener('change', saveOptions);
+  document.getElementById('selectedBackend').addEventListener('change', saveOptions);
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
