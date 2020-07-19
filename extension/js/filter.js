5117fdc9ea8e041205a06cda303d3395076c785d
artdgn <arthurdgn@gmail.com>
2020-07-19 22:45:55 +1000

    js style and usage fixups
     - add let and const where needed
     - add arrow function notation
     - move css to head in popup html
     - fetch instead of ajax
     - camel case variables
     - add strong to text tags

--- extension/js/filter.js
+++ extension/js/filter.js
@@ -1,53 +1,48 @@
 function findElements() {
-    return $("h1,h2,h3,h4,h5,p,span,li,a,img").filter(qualifyElement);
+    return $("h1,h2,h3,h4,h5,p,span,li,a,img,strong").filter(qualifyElement);
 }
 
-$.fn.immediateText = function() {
-    text = this.contents().not(this.children()).text();
+function immediateText(element) {
+    element = $(element);
+
+    let text = element.contents().not(element.children()).text();
 
     // handle img tags
     if (!text) {
-        alt_text = $(this).attr("alt");
-        text = (alt_text != null) ? text + ' ' + alt_text : text;
+        const altText = $(element).attr("alt");
+        text = (altText != null) ? `${text} ${altText}`: text;
     };
 
     // handle a tags
     if (!text) {
-        label_text = $(this).attr("label") ? $(this).attr("label") : "";
-        title_text = $(this).attr("title") ? $(this).attr("title") : "";
-        text = text + ' ' + label_text + ' ' + title_text
+        const labelText = $(element).attr("label") ? $(element).attr("label") : "";
+        const titleText = $(element).attr("title") ? $(element).attr("title") : "";
+        text = `${text} ${labelText} ${titleText}`
     };
 
     return text;
 };
 
 function qualifyElement(index, element) {
-    text = $(this).immediateText();
+    const text = immediateText(element);
 
     // words number
-    word_matches = text.match(/[\S]{3,}/g);
-    n_words = word_matches ? word_matches.length : 0;
+    const wordMatches = text.match(/[\S]{3,}/g);
+    const numWords = wordMatches ? wordMatches.length : 0;
 
-    return (n_words >= 3);
+    return (numWords >= 3);
 }
 
 function collectTexts(elements) {
-    texts = [];
-    elements.each(function (index) {
-        texts[index] = $(this).immediateText();
-    })
-    return texts;
+    return $.map(elements, (el) => immediateText(el));
 }
 
-
 function markByBackend(callback) {
-    elements = findElements();
+    const elements = findElements();
     chrome.storage.sync.get({
         backend: 'python',
     }, function(stored) {
-        backend = stored.backend;
-        console.log("stored backend setting: " + backend);
-        if (backend == "simple") {
+        if (stored.backend == "simple") {
             simpleHeuristicBackend(elements, callback);
         } else {
             pythonSentimentBackend(elements, callback);
@@ -56,121 +51,112 @@ function markByBackend(callback) {
 }
 
 function pythonSentimentBackend(elements, callback) {
-    texts = collectTexts(elements);
-    $.ajax({
-        type: 'post',
-        url: 'http://localhost:8000/sentiment/',
-        data: JSON.stringify({'texts': texts}),
-        success: function(data, status) {
-            setSentimentData(elements, data.values, data.ranks);
-            callback();
-        },
-        error: function(xhr, status, error) {
-            alert(xhr.responseText);
-        }
+    const texts = collectTexts(elements);
+    fetch('http://localhost:8000/sentiment/', {
+        method: 'post',
+        body: JSON.stringify({'texts': texts})
+    }).then(response => {
+        if (response.status !== 200) {
+            alert(`Backed returned error: ${response.status}`);
+            return;
+        };
+        return response.json();
+    }).then(data => {
+        setSentimentData(elements, data.values, data.ranks);
+        callback();
+    }).catch((error) => {
+        alert(`Backed call failed: ${error}`);
     });
 }
 
 function simpleHeuristicBackend(elements, callback) {
-    texts = collectTexts(elements);
-    values = [];
-    texts.forEach(function (text, index) {
-        word_matches = text.match(/[\S]{3,}/g);
-        n_words = word_matches ? word_matches.length : 0;
+    const texts = collectTexts(elements);
+    const values = [];
+    texts.forEach((text, index) => {
+        // all words
+        const wordMatches = text.match(/[\S]{3,}/g);
+        const numWords = wordMatches ? wordMatches.length : 0;
 
-        if (n_words == 0) {
-            // shouldn't happen
-            value = 0;
-        } else {
-            bad_words_matches = text.match(/trump|covid|coronavirus|pandemic/ig);
-            n_bad = bad_words_matches ? bad_words_matches.length : 0;
-            value = n_bad / n_words;
-        }
-        values[index] = value;
+        // bad words
+        const negativeMatches = text.match(/trump|covid|coronavirus|pandemic/ig);
+        const numNegatives = negativeMatches ? negativeMatches.length : 0;
+
+        values[index] = numNegatives / numWords;
     });
     setSentimentData(elements, values, arrayDenseRanks(values));
     callback();
 }
 
 function arrayDenseRanks(arr) {
-    sorted = Array.from(new Set(arr)).sort(function(a, b) {
-        return a - b;
-    });
-    ranks = arr.map(function(v) {
-        return sorted.indexOf(v) + 1;
-    });
+    const sorted = Array.from(new Set(arr)).sort((a, b) => (a - b));
+    const ranks = arr.map((v) => (sorted.indexOf(v) + 1));
     return ranks;
 }
 
-function setSentimentData(elements, neg_values, neg_ranks) {
-    max_rank = Math.max.apply(null, neg_ranks);
-    elements.each(function (index) {
-        $(this).addClass('negativityText');
-        $(this).attr('data-negativity-value', neg_values[index]);
-        $(this).attr('data-negativity-rank', neg_ranks[index] / max_rank);
+function setSentimentData(elements, negValues, negRanks) {
+    const maxRank = Math.max.apply(null, negRanks);
+    elements.each((i, el) => {
+        $(el).addClass('negativityText');
+        $(el).attr('data-negativity-value', negValues[i]);
+        $(el).attr('data-negativity-rank', negRanks[i] / maxRank);
     });
 }
 
 function adjustStyle() {
-    neg_margin = 0.0;
-    pos_margin = 0.0;
-    min_opacity = 0.1
+    const minOpacity = 0.1;
 
     chrome.storage.sync.get({
        styling: 'opacity',
        threshold: 50,
        ranking: false
-    }, function(stored) {
-       console.log("stored settings:");
-       console.log(stored);
+    }, (stored) => {
+       const threshold = Math.max(Math.min(stored.threshold / 100, 99.9), 0.01);
 
-       threshold = stored.threshold / 100;
-       neg_threshold = Math.min(threshold + neg_margin, 99.9);
-       pos_threshold = Math.max(threshold - pos_margin, 0.01);
-
-       $(".negativityText").each(function () {
+       $(".negativityText").each((i, el) => {
+           let score;
            if (stored.ranking) {
-               neg_score = parseFloat($(this).attr("data-negativity-rank"));
+               score = parseFloat($(el).attr("data-negativity-rank"));
            } else {
-               neg_score = parseFloat($(this).attr("data-negativity-value"));
+               score = parseFloat($(el).attr("data-negativity-value"));
            };
 
-           if ((stored.styling == "opacity") && (neg_score >= neg_threshold)) {
+           if ((stored.styling == "opacity") && (score >= threshold)) {
                 // opacity
-               norm_score = (neg_score - neg_threshold) / (1 - neg_threshold);
-               opacity = 1 - norm_score * (1 - min_opacity);
-               $(this).css('opacity', opacity);
+               const normScore = (score - threshold) / (1 - threshold);
+               const opacity = 1 - normScore * (1 - minOpacity);
+               $(el).css('opacity', opacity);
            } else {
-               $(this).css('opacity', 1.0);
+               $(el).css('opacity', 1.0);
            };
 
            // color
+           const maxColor = 200;
            if (stored.styling == "color") {
-               if (neg_score >= neg_threshold) {
-                   max_red = 200;
-                   norm_score = (neg_score - neg_threshold) / (1 - neg_threshold);
-                   color_val = Math.round(max_red * Math.pow(norm_score, 3));
-                   $(this).css('background-color',
-                               `rgba(255, ${max_red - color_val}, ${max_red - color_val}, 0.4)`);
-               } else if (neg_score < pos_threshold) {
-                   max_green = 200;
-                   norm_score = (pos_threshold - neg_score) / pos_threshold;
-                   color_val = Math.round(max_green * Math.pow(norm_score, 3));
-                   $(this).css('background-color',
-                               `rgba(${max_green - color_val}, 255, ${max_green - color_val}, 0.4)`);
-               }
+               if (score >= threshold) {
+                   const normScore = (score - threshold) / (1 - threshold);
+                   const colorVal = Math.round(maxColor * Math.pow(normScore, 3));
+                   $(el).css('background-color',
+                               `rgba(255, ${maxColor - colorVal},
+                                     ${maxColor - colorVal}, 0.4)`);
+               } else {
+                   const normScore = (threshold - score) / threshold;
+                   const colorVal = Math.round(maxColor * Math.pow(normScore, 3));
+                   $(el).css('background-color',
+                             `rgba(${maxColor - colorVal}, 255,
+                                   ${maxColor - colorVal}, 0.4)`);
+               };
            } else {
-               $(this).css('background-color', "inherit");
+               $(el).css('background-color', "inherit");
            };
 
-           structural = "div,tr,nav"
-           if ((stored.styling == "remove") && (neg_score >= neg_threshold)) {
+           const structural = "div,tr,nav";
+           if ((stored.styling == "remove") && (score >= threshold)) {
                 // remove
-               $(this).hide(100);
-               $(this).closest(structural).hide(100);
+               $(el).hide(100);
+               $(el).closest(structural).hide(100);
            } else {
-               $(this).show(100);
-               $(this).closest(structural).show(100)
+               $(el).show(100);
+               $(el).closest(structural).show(100);
            };
        });
     });
@@ -180,26 +166,25 @@ function adjustStyle() {
 markByBackend(adjustStyle);
 
 // watch for option changes
-chrome.storage.onChanged.addListener(function(changes) {
+chrome.storage.onChanged.addListener((changes) => {
     console.log(changes);
     if (changes.backend != null) {
         markByBackend(adjustStyle);
     } else {
         adjustStyle();
     };
-})
+});
 
 // watch for dynamically added elements (infinite scroll / twitter load)
-added = 0;
-observer = new MutationObserver(function(mutationsList, observer) {
+let added = 0;
+const observer = new MutationObserver((mutationsList, observer) => {
     for(let mutation of mutationsList) {
         added += mutation.addedNodes.length;
-    }
+    };
     if (added >= 20) {
         markByBackend(adjustStyle);
         added = 0;
-    }
+    };
 });
-observer.observe(document.body, {attributes: false,
-                                 childList: true,
-                                 subtree: true});
+observer.observe(
+    document.body, {attributes: false, childList: true, subtree: true});
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,39 +1,34 @@
 function saveOptions() {
-  options = {
-    styling: document.getElementById('selectedStyling').value,
-    backend: document.getElementById('selectedBackend').value,
-    threshold: document.getElementById('selectedThreshold').value,
-    ranking: document.getElementById('selectedRankingCheck').checked
-  };
-  chrome.storage.sync.set(options, function() {
-    console.log(options);
-  });
+    const options = {
+        styling: document.getElementById('selected-styling').value,
+        backend: document.getElementById('selected-backend').value,
+        threshold: document.getElementById('selected-threshold').value,
+        ranking: document.getElementById('selected-ranking-check').checked
+    };
+    chrome.storage.sync.set(options, () => void console.log(options));
 }
 
 function setFromStored(stored) {
-    document.getElementById('selectedStyling').value = stored.styling;
-    document.getElementById('selectedBackend').value = stored.backend;
-    document.getElementById('selectedThreshold').value = stored.threshold;
-    document.getElementById('selectedRankingCheck').checked = stored.ranking;
+    document.getElementById('selected-styling').value = stored.styling;
+    document.getElementById('selected-backend').value = stored.backend;
+    document.getElementById('selected-threshold').value = stored.threshold;
+    document.getElementById('selected-ranking-check').checked = stored.ranking;
 }
 
-
 function getOptions(callback) {
-  chrome.storage.sync.get({
-    styling: 'opacity',
-    backend: 'python',
-    threshold: 50,
-    ranking: false
-  }, function(stored) {
-    callback(stored);
-  });
+    chrome.storage.sync.get({
+        styling: 'opacity',
+        backend: 'python',
+        threshold: 50,
+        ranking: false
+    }, (stored) => void callback(stored));
 }
 
 function restoreOptions() {
-  getOptions(setFromStored);
-  for (element of document.getElementsByClassName('storedOptions')) {
-    element.addEventListener('change', saveOptions);
-  };
+    getOptions(setFromStored);
+    for (element of document.getElementsByClassName('stored-options')) {
+        element.addEventListener('change', saveOptions);
+    };
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
